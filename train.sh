f0529006e2309f77636756c6cbbeae749b445a48
Junhee Lee <62004821+junhee98@users.noreply.github.com>
2022-11-05 00:22:55 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -3,6 +3,6 @@
 DATASET="coffeecup"
 DATASET_MODEL="coffeecup"
 MODEL_CKPT="./exp_result/checkpoint1/checkpoint1test_lr_001_SGD_class4_freeze34-2_new_ver2.pt"
-CKPT_PATH="/home/jjunhe98/Desktop/kb/exp_result/checkpoint1/"
+CKPT_PATH="/home/jjunhee98/kb/exp_result/checkpoint1/"
 CKPT_NAME="checkpoint1test_lr_001_SGD_class4_freeze34-2_new_ver2.pt"
 CUDA_VISIBLE_DEVICES=0 /home/jjunhe98/anaconda3/envs/py38/bin/python infer.py $DATASET $DATASET_MODEL --model_ckpt $MODEL_CKPT $CKPT_PATH $CKPT_NAME
